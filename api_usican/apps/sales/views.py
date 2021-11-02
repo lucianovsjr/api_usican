dfacc0d38b7d13c060a97b0846adcf43c67177a0
lucianovsjr <lucianovsjr@gmail.com>
2021-11-02 19:16:39 -0300

    Fix Customer and Product model

--- api_usican/apps/sales/views.py
+++ api_usican/apps/sales/views.py
@@ -1,11 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
+from api_usican.misc.views import BaseModelViewSet
 from .models import customer
 from .serializers import customerSerializer
 
 
-class customerView(ModelViewSet):
+class customerView(BaseModelViewSet):
     queryset = customer.objects.all()
     serializer_class = customerSerializer
     permission_classes = [IsAuthenticated]
