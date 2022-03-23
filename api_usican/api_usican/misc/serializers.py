2a4b40cf3a410e453d620ed86a169e2cdd73ec07
lucianovsjr <lucianovsjr@gmail.com>
2022-03-23 16:47:13 -0300

    Fix active field validation when changing record

--- api_usican/api_usican/misc/serializers.py
+++ api_usican/api_usican/misc/serializers.py
@@ -5,9 +5,12 @@ class BaseRegisterSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
 
-        if not data["active"] and not self.instance.active:
-            raise serializers.ValidationError(
-                {"message": f"usican.error.{self.instance.get_model()}.active_is_false"}
-            )
+        if self.instance:
+            if not data.get("active", False) and not self.instance.active:
+                raise serializers.ValidationError(
+                    {
+                        "message": f"usican.error.{self.instance.get_model()}.active_is_false"
+                    }
+                )
 
         return data
